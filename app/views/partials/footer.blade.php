f11989aa21a34a89df4e3c49cbd338edf1b17258
YongHun Byun <yonghunbyun@gmail.com>
2014-04-02 01:38:59 +0900

    페이지 하단 부분 수정

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
 <div id="footer">
   <div class="container">
-    <p class="text-muted credit">Website built with <a href="http://laravel.com">Laravel</a> by <a target="_blank" href="http://twitter.com/stidges">Stidges</a> &amp; <a target="_blank" href="http://twitter.com/msurguy">Maks Surguy</a> | <a href="{{ url('about') }}">About</a>
+    <p class="text-muted credit">Website built with <a href="http://laravel.com">Laravel</a> by <a target="_blank" href="http://about.me/yhbyun">YongHun Byun</a>
     <span class="pull-right">
-        <a target="_blank" href="http://twitter.com/laraveltricks" title="Follow updates"><i class="fa fa-twitter fa-lg"></i></a>
+        <a target="_blank" href="http://twitter.com/laravelrocks" title="Follow updates"><i class="fa fa-twitter fa-lg"></i></a>
         |
-        <a target="_blank" href="https://github.com/CodepadME/laravel-tricks" title="Get the source of this site"><i class="fa fa-github fa-lg"></i></a>
+        <a target="_blank" href="https://github.com/yhbyun/laravel-tricks" title="Get the source of this site"><i class="fa fa-github fa-lg"></i></a>
     </span>
     </p>
 
