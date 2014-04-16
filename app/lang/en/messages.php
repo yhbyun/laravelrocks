36272d9ef20e6efc93b8071c408f4b4fcc1b604c
YongHun Byun <yonghunbyun@gmail.com>
2014-04-16 10:29:06 +0900

    Added English message file

---
+++ app/lang/en/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	'login'         => '로그인',
+	'logout'        => '로그아웃',
+	'email'         => '이메일',
+	'username'      => '사용자 아이디',
+	'profile-picture' => '프로필 사진',
+	'password'      => '비밀번호',
+	'repassword'    => '비밀번호 확인',
+
+	'rememberme'    => '로그인 상태 유지',
+	'lostpassword'  => '비밀번호 분실',
+	'signup'        => '회원가입',
+
+	'settings'      => '설정',
+
+	'title.recent-browse'       => '최근 등록한 Laravel 팁 보기',
+	'title.popular-browse'      => '인기있는 Laravel 팁 보기',
+	'title.commented-browse'    => '댓글이 많이 달린 Laravel 팁 보기',
+
+	'already-signed' => '회원이십니까?',
+
+	'signup.welcome' => '<h4>Welcome to Laravel tricks!</h4><p>Explore the tricks, create and share some of your own!</p>',
+
+);
