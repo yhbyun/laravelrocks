810ca5bac971b158dc01a5bb530003f674070420
YongHun Byun <yonghunbyun@gmail.com>
2014-04-16 10:31:03 +0900

    Patched Laravel 4.1.26

--- app/Tricks/User.php
+++ app/Tricks/User.php
@@ -122,4 +122,19 @@ class User extends Model implements UserInterface, RemindableInterface
 	{
 		return ($this->is_admin == true);
 	}
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
