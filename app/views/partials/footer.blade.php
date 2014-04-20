f0eeef1e010aca01953d7729868e56444b2c8f27
YongHun Byun <yonghunbyun@gmail.com>
2014-04-20 11:17:31 +0900

    Changed laravelrocks source code url

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
     <span class="pull-right">
         <a target="_blank" href="http://twitter.com/laravelrocks" title="Follow updates"><i class="fa fa-twitter fa-lg"></i></a>
         |
-        <a target="_blank" href="https://github.com/yhbyun/laravel-tricks" title="Get the source of this site"><i class="fa fa-github fa-lg"></i></a>
+        <a target="_blank" href="https://github.com/yhbyun/laravelrocks" title="Get the source of this site"><i class="fa fa-github fa-lg"></i></a>
         |
         <a href="{{ route('feed.atom') }}" tile="RSS feed"><i class="fa fa-rss fa-lg"></i></a>
     </span>
