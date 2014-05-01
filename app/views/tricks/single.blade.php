b26bb7b23e275c47890b0dfec767dcb0d80b8817
YongHun Byun <yonghunbyun@gmail.com>
2014-05-02 00:01:24 +0900

    Add google adsense banner

--- app/views/tricks/single.blade.php
+++ app/views/tricks/single.blade.php
@@ -51,6 +51,17 @@
                     @if ($trick->code)
                     <pre><code class="php">{{{ $trick->code }}}</code></pre>
                     @endif
+					<div class="text-center" style="margin:20px">
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- laravelrocks - 본문 -->
+					<ins class="adsbygoogle"
+						 style="display:inline-block;width:728px;height:90px"
+						 data-ad-client="ca-pub-6475195488182163"
+						 data-ad-slot="1361066067"></ins>
+					<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+					</div>
                 </div>
             </div>
                 <div class="col-lg-3 col-md-4">
