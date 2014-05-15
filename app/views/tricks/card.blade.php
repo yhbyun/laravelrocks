2525d8203e62672c68a55164b4eea79e8594c5ff
YongHun Byun <yonghunbyun@gmail.com>
2014-05-15 21:57:18 +0900

    Add draft class in draft trick

--- app/views/tricks/card.blade.php
+++ app/views/tricks/card.blade.php
@@ -1,4 +1,4 @@
-<div href="#" class="trick-card col-lg-4 col-md-6 col-sm-6 col-xs-12">
+<div href="#" class="trick-card {{ $trick->draft ? 'draft' : '' }} col-lg-4 col-md-6 col-sm-6 col-xs-12">
 	<div class="trick-card-inner js-goto-trick" data-slug="{{ $trick->id . '-' . $trick->slug }}">
 		<a class="trick-card-title" href="{{ route('tricks.show', [$trick->id, $trick->slug]) }}">
 			{{{ $trick->title }}}
