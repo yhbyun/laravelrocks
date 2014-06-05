70a729a376c39aa83051bc74c4721d0d8929835b
YongHun Byun <yonghunbyun@gmail.com>
2014-06-06 05:21:51 +0900

    Fix the closing tag error

--- app/lang/ko/feeds.php
+++ app/lang/ko/feeds.php
@@ -6,7 +6,7 @@ return array(
 	'sub_title' => 'Laravel rocks는 Laravel 관련 팁을 모아 놓은 사이트입니다',
 	'author' => '
 	    <author>
-		<name>YongHun Byun/name>
+		<name>YongHun Byun</name>
 		<uri>http://twitter.com/river</uri>
 	    </author>',
 	'language' => 'ko-kr',
