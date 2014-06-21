a1d41240c165117aa81fc672b51cd29b1fa057bd
YongHun Byun <yonghunbyun@gmail.com>
2014-06-21 17:49:34 +0900

    Remove linke menu

--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -18,8 +18,6 @@
 
 				{{ Navigation::make(Request::path()) }}
 
-				<li class="hidden-xs"><a href="http://laravel.link/">Laravel.link</a></li>
-
 				@if(Auth::guest())
 					<li class="visible-xs"><a href="{{ url('register') }}">{{ trans('partials.register') }}</a></li>
 					<li class="visible-xs"><a href="{{ url('login') }}">{{ trans('partials.login') }}</a></li>
